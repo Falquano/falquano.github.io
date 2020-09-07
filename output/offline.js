b7b68999c73646127ddaa9aa963c1b770e9c25f9
Falquano <34924964+Falquano@users.noreply.github.com>
2020-09-07 13:26:59 +0200

    Add files via upload

---
+++ output/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1599477873,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/tiledbackground.png",
+		"images/pic-sheet0.png",
+		"images/decayblock-sheet0.png",
+		"images/door-sheet0.png",
+		"media/jump.m4a",
+		"media/jump.ogg",
+		"media/door.m4a",
+		"media/door.ogg",
+		"media/death.m4a",
+		"media/death.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
