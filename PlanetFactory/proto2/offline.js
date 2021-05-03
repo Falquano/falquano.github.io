07a4edee9b79b7c9e43f6f125b3a3af8e0f7875f
Falquano <34924964+Falquano@users.noreply.github.com>
2021-05-03 18:44:07 +0200

    Add files via upload

---
+++ PlanetFactory/proto2/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1620059261,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/boule1-sheet0.png",
+		"images/boule2-sheet0.png",
+		"images/boule3-sheet0.png",
+		"images/boule4-sheet0.png",
+		"images/boule5-sheet0.png",
+		"images/viseur-sheet0.png",
+		"images/noyau-sheet0.png",
+		"images/atmosphère-sheet0.png",
+		"images/scorecounter.png",
+		"images/next-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
